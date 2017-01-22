da5d5a5466387872ea7359cb57eb04bdf1b37d35
And2X <Andrew.android.yandex@gmail.com>
2017-01-22 18:29:16 +0300

    3.3 static mock layout.html

---
+++ js/spa.js
@@ -0,0 +1,22 @@
+//root module
+
+/*jslint browser:true, continue:true,
+    devel:true, indent:2, maxerr:50,
+    newcap:true, nomen:true, plusplus:true,
+    regexp:true, sloppy:true, vars:true,
+    white:true
+    */
+/*global $, spa */
+
+var spa = (function(){
+    var initModule = function($container){
+        $container.html(
+        '<h1 style="dsplay:inline-block; margin: 25px;">'
+        + 'hello, World!'
+            + '</h1>'
+        );
+    };
+    return {
+        initModule: initModule
+    };
+}());
